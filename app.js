4a4dd19845dfc0b7b6a3d028b3132bd38431154d
Maximo Busilacchi <k51538@frro.utn.edu.ar>
2025-08-21 21:39:01 -0300

    Creo listarAmigos()

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let listaAmigos = [];
+let amigos = [];
 function agregarAmigo() {
     let nombreAmigo = document.getElementById('amigo').value;
     if(nombreAmigo == ""){
         alert("Por favor, inserte un nombre.");
     }
     else{
-        listaAmigos.push(nombreAmigo);
+        amigos.push(nombreAmigo);
         limpiarCaja();
+        listarAmigos();
         return;
     }
 }    
 function limpiarCaja() {
     document.querySelector('#amigo').value = '';
 }
-console.log(listaAmigos);
+console.log(amigos);    
+
+
+function listarAmigos(){
+    let lista = document.querySelector('#listaAmigos');
+    lista.innerHTML = "";
+    for (i=0; i < amigos.length; i++){
+        lista.innerHTML += `<li>${amigos[i]}</li>`;
+    }
+}   
+
